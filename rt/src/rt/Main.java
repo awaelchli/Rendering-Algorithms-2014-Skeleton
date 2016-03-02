88af00563120d4f8b19bf41da5eacab997c9c2d6
Adrian Wälchli <adrian.waelchli@students.unibe.ch>
2016-03-02 20:53:09 +0100

    implemented csgsphere

--- rt/src/rt/Main.java
+++ rt/src/rt/Main.java
@@ -24,7 +24,7 @@ public class Main {
 	/** 
 	 * The scene to be rendered.
 	 */
-	public static Scene scene = new CameraTestScene();
+	public static Scene scene = new InstancingTest();
 	/**
 	 * A render task represents a rectangular image region that is rendered
 	 * by a thread in one chunk.
--- rt/src/rt/intersectables/CSGSphere.java
+++ rt/src/rt/intersectables/CSGSphere.java
@@ -1,7 +1,11 @@
 package rt.intersectables;
 
+import rt.HitRecord;
+import rt.Material;
 import rt.Ray;
 
+import javax.vecmath.Point3f;
+import javax.vecmath.Vector3f;
 import java.util.ArrayList;
 
 /**
@@ -9,12 +13,81 @@ import java.util.ArrayList;
  */
 public class CSGSphere extends CSGSolid {
 
-    public CSGSphere(){
+    Vector3f center;
+    float radius;
 
+    public Material material;
+
+    public CSGSphere(Vector3f center, float radius){
+        this.center = center;
+        this.radius = radius;
     }
 
     @Override
     ArrayList<IntervalBoundary> getIntervalBoundaries(Ray r) {
-        return null;
+        // Notation following http://www.siggraph.org/education/materials/HyperGraph/raytrace/rtinter1.htm
+
+        Vector3f d = new Vector3f(r.direction);
+        d.normalize();
+        Point3f o = new Point3f(r.origin);
+
+        float b = 2 * (d.x * (o.x - center.x) + d.y * (o.y - center.y) + d.z * (o.z - center.z));
+        float c1 = (o.x - center.x);
+        float c2 = (o.y - center.y);
+        float c3 = (o.z - center.z);
+        float c = c1 * c1 + c2 * c2 + c3 * c3 - radius * radius;
+
+        // a = 1
+        float discriminant = b * b - 4 * c;
+
+        if (discriminant < 0){
+            // No intersection
+            return null;
+        }
+
+        // t-Parameters at boundaries
+        float t1 = (-b - (float) Math.sqrt(discriminant)) / 2;
+        float t2 = (-b + (float) Math.sqrt(discriminant)) / 2;
+
+        // Position of first boundary
+        Point3f position1 = r.pointAt(t1);
+
+        // Position of second boundary
+        Point3f position2 = r.pointAt(t2);
+
+        // Normal at first boundary
+        Vector3f normal1 = new Vector3f(position1);
+        normal1.sub(this.center);
+        normal1.normalize();
+
+        // Normal at second boundary
+        Vector3f normal2 = new Vector3f(normal1);
+        normal2.negate();
+
+        // Ray direction
+        Vector3f w = new Vector3f(d);
+        w.negate();
+
+        float u = 0, v = 0;
+        HitRecord hit1 = new HitRecord(t1, position1, normal1, w, this, this.material, u, v);
+        HitRecord hit2 = new HitRecord(t2, position2, normal2, w, this, this.material, u, v);
+
+        // First Boundary
+        IntervalBoundary b1 = new IntervalBoundary();
+        b1.type = BoundaryType.START;
+        b1.t = t1;
+        b1.hitRecord = hit1;
+
+        // Second Boundary
+        IntervalBoundary b2 = new IntervalBoundary();
+        b2.type = BoundaryType.END;
+        b2.t = t2;
+        b2.hitRecord = hit2;
+
+        ArrayList<IntervalBoundary> boundaries = new ArrayList<>();
+        boundaries.add(b1);
+        boundaries.add(b2);
+
+        return boundaries;
     }
 }
