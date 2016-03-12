dfa603cd2ad9a40f99956d688ccec5fa8527f0f0
Adrian Wälchli <adrian.waelchli@students.unibe.ch>
2016-03-12 19:24:47 +0100

    fixed some problems

--- rt/src/rt/materials/BlinnPlusMirror.java
+++ rt/src/rt/materials/BlinnPlusMirror.java
@@ -2,6 +2,7 @@ package rt.materials;
 
 import rt.HitRecord;
 import rt.Material;
+import rt.Ray;
 import rt.Spectrum;
 
 import javax.vecmath.Vector3f;
@@ -14,27 +15,9 @@ public class BlinnPlusMirror implements Material {
     Blinn blinn;
     Spectrum reflectivity;
 
-    private Spectrum f;
-
     public  BlinnPlusMirror(Spectrum kd, Spectrum ks, float shininess, Spectrum reflectivity) {
         blinn = new Blinn(kd, ks, shininess);
         this.reflectivity = reflectivity;
-
-        // Precompute the constant f for Schlick's approximation
-        schlick_precompute(reflectivity);
-    }
-
-    private void schlick_precompute(Spectrum reflectivity) {
-        float r_r = reflectivity.r;
-        float r_g = reflectivity.g;
-        float r_b = reflectivity.b;
-
-        float f_r = (1 - r_r) / (1 + r_r);
-        float f_g = (1 - r_g) / (1 + r_g);
-        float f_b = (1 - r_b) / (1 + r_b);
-
-        this.f = new Spectrum(f_r, f_g, f_b);
-        this.f.mult(f);
     }
 
     @Override
@@ -54,20 +37,11 @@ public class BlinnPlusMirror implements Material {
 
     @Override
     public ShadingSample evaluateSpecularReflection(HitRecord hitRecord) {
-        // Using Schlick's approximation:
-        // r = f + (1 - f) * (1 - w dot n)^5
-        float nDotw = hitRecord.normal.dot(hitRecord.w);
-        float x = 1 - nDotw;
-        x = x * x * x * x * x;
-        Spectrum r = new Spectrum(f);
-        r.mult(-1);
-        r.add(new Spectrum(1, 1, 1));
-        r.mult(x);
-        r.add(f);
+        Ray reflectedRay = Ray.reflect(hitRecord);
         ShadingSample s = new ShadingSample();
-        //s.brdf = r;
-        //s.brdf = blinn.ks;
-        s.brdf = reflectivity;
+        s.brdf = new Spectrum(reflectivity);
+        s.w = reflectedRay.direction;
+        s.p = 1;
         return s;
     }
 
@@ -93,6 +67,6 @@ public class BlinnPlusMirror implements Material {
 
     @Override
     public boolean castsShadows() {
-        return false;
+        return true;
     }
 }
