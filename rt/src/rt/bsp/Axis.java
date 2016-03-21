575a7ce7d129978624cf186d2be2a7c7ec303272
Adrian Wälchli <adrian.waelchli@students.unibe.ch>
2016-03-21 22:06:10 +0100

    make methods non-static

--- rt/src/rt/bsp/Axis.java
+++ rt/src/rt/bsp/Axis.java
@@ -1,6 +1,7 @@
 package rt.bsp;
 
 import javax.vecmath.Tuple3f;
+import javax.vecmath.Vector3f;
 
 /**
  * Simple enum to identify each axis.
@@ -21,8 +22,29 @@ enum Axis {
         return null;
     }
 
-    static float getValueFromAxis(Tuple3f tuple, Axis axis) {
-        switch (axis) {
+    Vector3f getUnitVector() {
+        switch (this) {
+            case X:
+                return new Vector3f(1, 0, 0);
+            case Y:
+                return new Vector3f(0, 1, 0);
+            case Z:
+                return new Vector3f(0, 0, 1);
+        }
+        return null;
+    }
+
+    int getIndex() {
+        switch (this) {
+            case X: return 0;
+            case Y: return 1;
+            case Z: return 2;
+        }
+        return -1;
+    }
+
+    float getValue(Tuple3f tuple) {
+        switch (this) {
             case X:
                 return tuple.x;
             case Y:
