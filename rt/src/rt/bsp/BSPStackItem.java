73cc95d7af91d7762d9f02a4c7cc1fc3082f21ef
awaelchli <aedu.waelchli@gmail.com>
2016-03-22 16:00:48 +0100

    created stackitem

---
+++ rt/src/rt/bsp/BSPStackItem.java
@@ -0,0 +1,10 @@
+package rt.bsp;
+
+/**
+ * A simple stack item used by {@link BSPAccelerator} to calculate intersections.
+ */
+public class BSPStackItem
+{
+    BSPNode node;
+    float tmin, tmax;
+}
