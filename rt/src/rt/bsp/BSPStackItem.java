a2c75e60910e35dace5e7015dc92b22983d885d7
Adrian Wälchli <adrian.waelchli@students.unibe.ch>
2016-03-24 00:10:56 +0100

    moved inside accelerator class

--- rt/src/rt/bsp/BSPStackItem.java
+++ rt/src/rt/bsp/BSPStackItem.java
@@ -1,10 +1,2 @@
 package rt.bsp;
 
-/**
- * A simple stack item used by {@link BSPAccelerator} to calculate intersections.
- */
-public class BSPStackItem
-{
-    BSPNode node;
-    float tmin, tmax, tsplit;
-}
