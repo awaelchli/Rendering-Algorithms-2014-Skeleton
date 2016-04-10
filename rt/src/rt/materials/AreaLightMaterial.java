ca357f35d039004d761456fb5f78a75adcb327a8
Adrian Wälchli <adrian.waelchli@students.unibe.ch>
2016-04-10 23:16:30 +0200

    the material for area lights

---
+++ rt/src/rt/materials/AreaLightMaterial.java
@@ -0,0 +1,79 @@
+package rt.materials;
+
+import rt.HitRecord;
+import rt.Material;
+import rt.Spectrum;
+
+import javax.vecmath.Vector3f;
+
+/**
+ * Created by Adrian on 10.04.2016.
+ */
+public class AreaLightMaterial implements Material
+{
+    Spectrum power;
+    float area;
+
+    public AreaLightMaterial(Spectrum power, float area)
+    {
+        this.power = power;
+        this.area = area;
+    }
+
+    @Override
+    public Spectrum evaluateBRDF(HitRecord hitRecord, Vector3f wOut, Vector3f wIn)
+    {
+        return new Spectrum(power);
+    }
+
+    @Override
+    public Spectrum evaluateEmission(HitRecord hitRecord, Vector3f wOut)
+    {
+        Spectrum emission = new Spectrum(power);
+        emission.mult(1 / area);
+        emission.mult((float) (1 / Math.PI));
+        return emission;
+    }
+
+    @Override
+    public boolean hasSpecularReflection()
+    {
+        return false;
+    }
+
+    @Override
+    public ShadingSample evaluateSpecularReflection(HitRecord hitRecord)
+    {
+        return null;
+    }
+
+    @Override
+    public boolean hasSpecularRefraction()
+    {
+        return false;
+    }
+
+    @Override
+    public ShadingSample evaluateSpecularRefraction(HitRecord hitRecord)
+    {
+        return null;
+    }
+
+    @Override
+    public ShadingSample getShadingSample(HitRecord hitRecord, float[] sample)
+    {
+        return null;
+    }
+
+    @Override
+    public ShadingSample getEmissionSample(HitRecord hitRecord, float[] sample)
+    {
+        return null;
+    }
+
+    @Override
+    public boolean castsShadows()
+    {
+        return false;
+    }
+}
