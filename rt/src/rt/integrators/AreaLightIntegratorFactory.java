1eaea3d12fa6cb2f192e181ed65ed83f3b65b3aa
Adrian Wälchli <adrian.waelchli@students.unibe.ch>
2016-04-17 22:55:14 +0200

    added sampling technique

--- rt/src/rt/integrators/AreaLightIntegratorFactory.java
+++ rt/src/rt/integrators/AreaLightIntegratorFactory.java
@@ -3,20 +3,32 @@ package rt.integrators;
 import rt.Integrator;
 import rt.Sampler;
 import rt.Scene;
+import rt.samplers.RandomSampler;
 
 /**
  * Makes a {@link PointLightIntegrator}.
  */
 public class AreaLightIntegratorFactory extends WhittedIntegratorFactory {
 
-    int numberOfSamples = 1;
+    int numberOfSamples;
     Sampler sampler;
+    private AreaLightIntegrator.SamplingTechnique samplingTechnique;
+
+    public AreaLightIntegratorFactory()
+    {
+        super();
+        this.numberOfSamples = 1;
+        this.sampler = new RandomSampler();
+        this.samplingTechnique = AreaLightIntegrator.SamplingTechnique.MIS;
+    }
 
     public Integrator make(Scene scene)
     {
-        AreaLightIntegrator integrator = new AreaLightIntegrator(scene, recursionDepth);
+        AreaLightIntegrator integrator = new AreaLightIntegrator(scene);
+        integrator.recursionDepth = this.recursionDepth;
         integrator.numberOfSamples = this.numberOfSamples;
         integrator.sampler = this.sampler;
+        integrator.samplingTechnique = this.samplingTechnique;
         return integrator;
     }
 
@@ -30,4 +42,8 @@ public class AreaLightIntegratorFactory extends WhittedIntegratorFactory {
         this.sampler = sampler;
     }
 
+    public void setSamplingTechnique(AreaLightIntegrator.SamplingTechnique samplingTechnique)
+    {
+        this.samplingTechnique = samplingTechnique;
+    }
 }
